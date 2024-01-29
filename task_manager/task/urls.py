12b34ea551f4eda52fdc26c722bc680607ead05b
Youssef Shaker <youssefaymanshaker@outlook.com>
2024-01-29 12:53:20 +0200

    mark task as undone

--- task_manager/task/urls.py
+++ task_manager/task/urls.py
@@ -5,5 +5,6 @@ from task import views
 urlpatterns = [
     path("add/", views.add_task, name="add-task"),
     path("mark-task-done/<int:pk>", views.mark_task_done, name="mark-task-done"),
+    path("mark-task-undone/<int:pk>", views.mark_task_undone, name="mark-task-undone"),
     path("remove/<int:pk>", views.remove_task, name="remove-task"),
 ]
--- task_manager/task/views.py
+++ task_manager/task/views.py
@@ -21,3 +21,10 @@ def mark_task_done(request, pk):
     task.is_completed = True
     task.save()
     return redirect("home")
+
+
+def mark_task_undone(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    task.is_completed = False
+    task.save()
+    return redirect("home")
